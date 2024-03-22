cb86f5e77f05716bde9980c70f7c2bc7adc65208
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2024-03-22 14:35:23 +0200

    Add coverage report

--- tasks.py
+++ tasks.py
@@ -15,3 +15,15 @@ def format(ctx):
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=pty)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=pty)
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=pty)
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=pty)
