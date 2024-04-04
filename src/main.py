29502bc4006e8c7db5a32f524ae4133840175f40
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2024-04-04 11:53:15 +0300

    Add basic ui

--- src/main.py
+++ src/main.py
@@ -1,5 +1,34 @@
 import huffman
 
 
+def huffman_coding(filename):
+
+    huffman_algorithm = huffman.HuffmanCoding(f"src/{filename}")
+    while True:
+        action = input(
+            "Press C: compress \npress D: decompress\npress E: exit\n").upper()
+        if action == "C":
+            huffman_algorithm.compress()
+            print("Compressed")
+        if action == "D":
+            huffman_algorithm.decompress()
+            print("Decompressed")
+        if action == "E":
+            break
+
+
+def main():
+    filename = input("Give name of the file ")
+    while True:
+        algorithm = input(
+            "Press H to Huffman coding and press E to exit: ").upper()
+        if algorithm == "H":
+            huffman_coding(filename)
+        if algorithm == "E":
+            break
+
+
 H = huffman.HuffmanCoding("src/text.txt")
 H.compress()
+H.decompress()
+# main()
