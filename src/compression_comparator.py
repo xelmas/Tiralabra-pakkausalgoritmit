f58ec72cd7b641449b380ae9733a3f340faaf2db
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2024-04-11 14:30:53 +0300

    Update docstring

--- src/compression_comparator.py
+++ src/compression_comparator.py
@@ -1,18 +1,20 @@
 
 class CompressionComparator():
-    """This class orchestrates the comparison process, including calling the compression and decompression methods 
-    of Algorithm1 and Algorithm2, measuring their performance, and providing the results to the UI for display.
+    """This class is used for comparing the algorithms, including calling the compression
+    and decompression methods of huffman coding algorithm and LZW-algorithm, 
+    measuring their performance, and providing the results to the UI for display.
     """
+
     def __init__(self, huffman, lzw) -> None:
-        
+
         self.huffman = huffman
         self.lzw = lzw
-    
+
     def compare(self):
         pass
 
     def compress(self, algorithm):
         algorithm.compress_file()
-        
+
     def decompress(self, algorithm):
         algorithm.decompress_file()
