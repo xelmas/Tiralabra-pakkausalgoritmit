ccebadd5838ff0338c4305433ddbb1ee7c8e1e00
Elisa Suorsa <84924097+xelmas@users.noreply.github.com>
2024-04-20 16:01:22 +0300

    Add feature to compare both algorithms

--- src/main.py
+++ src/main.py
@@ -11,14 +11,15 @@ def action_loop(ui, comparator, algorithm, prev_compress=False):
         if action == "C":
             comparator.compress(algorithm)
             prev_compress = True
+            ui.display_message("Compressed")
         if action == "D":
             if prev_compress:
                 comparator.decompress(algorithm)
+                ui.display_message("Decompressed")
             else:
                 ui.display_message("File must be compressed first")
         if action == "E":
             break
-
 
 def main():
     ui = UI()
@@ -29,9 +30,9 @@ def main():
             break
         ui.display_message("File not found")
 
-    huffman = HuffmanCoding(filehandler)
-    lzw = LZW(filehandler)
-    comparator = CompressionComparator(huffman, lzw)
+    huffman = HuffmanCoding()
+    lzw = LZW()
+    comparator = CompressionComparator(filehandler)
 
     while True:
         chosen_algorithm = ui.get_algorithm()
@@ -39,6 +40,10 @@ def main():
             action_loop(ui, comparator, huffman)
         if chosen_algorithm == "L":
             action_loop(ui, comparator, lzw)
+        if chosen_algorithm == "C":
+            ui.display_message("\nCalculating...")
+            ui.display_table(comparator.compare(huffman, lzw))
+            break
         if chosen_algorithm == "E":
             break
 
--- src/ui.py
+++ src/ui.py
@@ -1,18 +1,19 @@
 import os.path
+from prettytable import PrettyTable
 
 
 class UI:
 
     def get_file_path(self):
-        filename = input("Give name of the file: ")
+        filename = input("Give name of the file (e.g. sample.txt): ")
         if os.path.isfile(f"src/{filename}"):
             return f"src/{filename}"
         return None
 
     def get_algorithm(self):
-        print("\nChoose algorithm")
+        print("\nChoose one algorithm or both")
         algorithm = input(
-            "Press H: Huffman coding\nPress L: LZW\nPress E: Exit\n").upper()
+            "Press H: Huffman coding\nPress L: LZW\nPress C: Compare both\nPress E: Exit\n").upper()
         return algorithm
 
     def get_action(self):
@@ -23,3 +24,13 @@ class UI:
 
     def display_message(self, message):
         print(message)
+
+    def display_table(self, compression_stats):
+        table = PrettyTable()
+        table.field_names = ["Algorithm", "Compression time",
+                             "Decompression time", "Compression ratio"]
+
+        for stats in compression_stats:
+            if stats is not None:
+                table.add_row(stats)
+        print(str(table))
